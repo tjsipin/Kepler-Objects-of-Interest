85332d0840c2163b225f4561fc8ee0dc3b5d9251
TJ Sipin <tjsipin@gmail.com>
2022-03-12 21:53:18 -0800

    tj's tree works now

--- tree.R
+++ tree.R
@@ -17,11 +17,13 @@ tree_grid
 
 set.seed(234)
 
+# set koi_disposition as factor
+as.factor(koi$koi_disposition)
 
 set.seed(345)
 tree_wf <- workflow() %>%
   add_model(tune_spec) %>%
-  add_formula(koi_disposition ~ .)
+  add_recipe(recipe)
 
 tree_res <- tree_wf %>%
   tune_grid(resamples = koi_folds,
@@ -48,7 +50,7 @@ tree_res %>%
 best_tree <- tree_res %>%
   select_best("accuracy")
 
-best_tree
+best_tree # best tree has tree depth of 8
 
 
 final_tree_wf <- tree_wf %>%
@@ -62,3 +64,4 @@ final_tree_fit <-
 
 final_tree_fit %>%
   collect_metrics()
+
