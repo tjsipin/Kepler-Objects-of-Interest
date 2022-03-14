83226b0ada173a4afc663c40c976acb019c721f9
TJ Sipin <tjsipin@gmail.com>
2022-03-14 15:53:54 -0700

    dendogram added but tentative

--- PCA.R
+++ PCA.R
@@ -34,6 +34,8 @@ var_df %>%
 
 juice(pca_estimates)
 
+# Cumulative sum of explained variance
+
 plot(cumsum(percent_variation), xlab = "Principal Component",
      ylab = "Proportion of Variation Explained", 
      ylim = c(0,1), type = 'b') %>%
@@ -41,3 +43,24 @@ plot(cumsum(percent_variation), xlab = "Principal Component",
   grid(col = "lightgray", lty = "dotted")
 
 
+# Biplot of PC1 vs PC2
+
+plot(x = pr.out$x[, c(1, 2)][,1],
+     y = pr.out$x[, c(1, 2)][,2],
+     col = 1:3,
+     cex = 0.3)
+
+# 
+koi.dist <- dist(koi)
+koi.hclust <- hclust(koi.dist)
+dend3 = as.dendrogram(koi.hclust)
+dend3 = color_branches(dend3, k = 3)
+dend3 = color_labels(dend3, k = 3)
+
+dend3 = set(dend3, "labels_cex", 0.3)
+
+dend3 = set_labels(dend3, labels = koi$koi_disposition[order.dendrogram(dend3)])
+
+# Plot dendogram
+plot(dend3, horiz = T)
+     
